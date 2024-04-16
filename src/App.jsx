1ac84beefd6a991d15924dc024d15ef25d53faa3
Elie-mugisa <eliemugisa777@gmail.com>
2024-04-16 21:17:22 +0200

    Pages && Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Screens/Home";
+import SignIn from "./Screens/SignIn";
+import SignUp from "./Screens/SignUp";
+import Dashboard from './Screens/Dashboard';
+import Projects from "./Screens/Projects";
+
 function App() {
   return (
-    <>
-      <div className="bg-slate-500">Hello world</div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
